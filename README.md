7ac5ac305c32bcbbbc32f1cab0813df9da443d0e
Stephanie Fay <stephaniemfay@gmail.com>
2025-06-06 21:03:03 -0300

    initial commit with basic theme, navbar and loading scene

---
+++ README.md
@@ -0,0 +1,2 @@
+# s1-mvp-frontend
+Frontend part of the MVP for the first sprint (Software Engineer Specialization)
---
+++ js/app.js
@@ -0,0 +1,35 @@
+function changeTheme (theme) {
+  const themeDOM = document.getElementById("theme");
+
+  switch (theme) {
+    case 'light':
+      themeDOM.href = 'css/bootstrap-minty.css';
+      localStorage.setItem('theme', theme);
+      document.getElementById("theme-selected").src = 'img/sun.svg';
+      break;
+    case 'dark':
+      themeDOM.href = 'css/bootstrap-slate.css';
+      localStorage.setItem('theme', theme);
+      document.getElementById("theme-selected").src = 'img/moon.svg';
+      break;
+  }
+}
+
+function login () {
+  document.getElementById("login").classList.add("gone");
+  document.getElementById("logout").classList.remove("gone");
+  document.getElementById("wish").classList.remove("gone");
+}
+
+window.addEventListener('load', function () {
+  const currentTheme = localStorage.getItem('theme');
+  if (currentTheme != null) {
+    changeTheme(currentTheme);
+  }
+
+  document.getElementById("content").classList.remove('invisible');
+  document.getElementById("themeSelector").classList.remove('invisible');
+  document.getElementById("navbar").classList.remove('invisible');
+  document.getElementById("loading").classList.add('invisible');
+});
+
